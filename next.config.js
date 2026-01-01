eee9f7e389d80d83ebfb51883e19cac0e67b4d0c
hiperbro6-eng <hiperbro6@gmail.com>
2026-01-01 11:56:21 +0530

    fix: ensure dependencies are locked

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // This allows the build to finish even if there are small coding warnings
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  
+  
   typescript: {
     ignoreBuildErrors: true,
   },
-  // If you are using images from external sources
+
   images: {
     unoptimized: true,
   },
